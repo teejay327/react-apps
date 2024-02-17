4cb6152a2770a284045e153af3ad7373ea9fc03e
teejay327 <mrenergy@hotmail.com>
2024-02-18 00:24:31 +1000

    tidying up layout for Summary component

--- src/projects/weather-app-v2.0/Components/Summary.jsx
+++ src/projects/weather-app-v2.0/Components/Summary.jsx
@@ -1,23 +1,25 @@
 import DATA from '../data.js';
 import './Summary.css';
 
-const Summary = (props) => {
+const Summary = () => {
 
   return (
-    <div className='current-summary'>
-       <div className='current-summary-temperatures'>
+    <div className='summary'>
+       <div className='summary-temperatures'>
           <p>Currently</p>
-          <div>{ DATA.forecast.temperature } &#8451;</div>
+          <div>
+            { DATA.forecast.temperature } &#8451;
+         </div>
        </div>
-       <div className='current-summary-humidity'>
+       <div className='summary-humidity'>
           <p>Humidity</p>
           <div>{ DATA.forecast.humidity }</div>
        </div>
-       <div className='current-summary-rainfall'>
+       <div className='summary-rainfall'>
           <p>Rain since 9am</p>
           <div>{ DATA.forecast.rainfall }</div>
        </div>
-       <div className='current-summary-wind'>
+       <div className='summary-wind'>
           <p>Wind</p>
           <div>{ DATA.forecast.wind }</div>
        </div>
