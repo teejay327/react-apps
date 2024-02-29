799a18d597374166a7c849068a62f38dfff244ac
teejay327 <mrenergy@hotmail.com>
2024-02-29 23:47:09 +1000

    Tidied up UI for Statistics component

--- src/projects/weather-app-v2.0/Components/Statistics.jsx
+++ src/projects/weather-app-v2.0/Components/Statistics.jsx
@@ -1,5 +1,5 @@
 
-
+import DATA from '../data.js';
 import './Statistics.css';
 import uvIndex from '../assets/UVIndex.png';
 import wind from '../assets/Wind.png';
@@ -20,7 +20,7 @@ const Statistics = () => {
             Chance of rain:
           </div>
           <div className='stats-text-numbers'>
-            <div>PROPS.CHANCE</div>
+            <div>{ DATA.stats.chanceRain }</div>
           </div>
         </div>
       </div>
@@ -28,11 +28,11 @@ const Statistics = () => {
       <div className='statistics-section'>
         <div className='stats-icon'>
           <svg width="40px" height="40px" viewBox="0 0 46 46">
-            <g fill="none" fill-rule="evenodd">
+            <g fill="none" fillRule="evenodd">
               <circle fill="#98cfdc" cx="23" cy="23" r="23"></circle>
               <g transform="translate(15 9)">
-                <rect stroke="#000000" stroke-width="1.7" x="0.85" y="0.85" width="15.3" height="26.118" rx="1"></rect>
-                <path stroke="#000000" stroke-width="1.7" stroke-linecap="round" d="M11 11.5h5M11 16.5h5M11 6.5h5"></path>
+                <rect stroke="#000000" strokeWidth="1.7" x="0.85" y="0.85" width="15.3" height="26.118" rx="1"></rect>
+                <path stroke="#000000" strokeWidth="1.7" strokeLinecap="round" d="M11 11.5h5M11 16.5h5M11 6.5h5"></path>
                 <path d="M16 20.853c-.28.595-1.635.893-4.065.893C8.767 21.746 7.961 20 4.921 20c-1.316 0-2.623.284-3.921.853V27h15v-6.147z" fill="#000000"></path>
               </g>
             </g>
@@ -43,7 +43,7 @@ const Statistics = () => {
             Amount of rain:
           </div>
           <div className='stats-text-numbers'>
-            <div>PROPS.AMOUNT</div>
+            <div>{ DATA.stats.rainfall }</div>
           </div>
         </div>
       </div>
@@ -57,7 +57,7 @@ const Statistics = () => {
             UV Index:
           </div>
           <div className='stats-text-numbers'>
-            <div>PROPS.UV</div>
+            <div>{ DATA.stats.uvIndex }</div>
           </div>
         </div>
       </div>
@@ -71,7 +71,7 @@ const Statistics = () => {
             Wind speed:
           </div>
           <div className='stats-text-numbers'>
-            <div>PROPS.WIND</div>
+            <div>{ DATA.stats.wind }</div>
           </div>
         </div>
       </div>
