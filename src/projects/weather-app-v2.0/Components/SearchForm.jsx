ac9ab197ec3f954b7b1f46eb85f3572c87866f41
teejay327 <mrenergy@hotmail.com>
2024-03-04 23:44:33 +1000

    Added SearchForm component

---
+++ src/projects/weather-app-v2.0/Components/SearchForm.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './SearchForm.css';
+
+const SearchForm = (props) => {
+
+    return (
+      <form>
+        <div>
+          Search for a location in Australia
+        </div>
+      </form>
+
+      
+    )
+};
