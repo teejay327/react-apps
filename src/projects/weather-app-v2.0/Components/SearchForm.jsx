f6eebdea097a97c82a35b5829e83c3d41e202ced
teejay327 <mrenergy@hotmail.com>
2024-03-05 23:57:15 +1000

    Expanded SearchForm component

--- src/projects/weather-app-v2.0/Components/SearchForm.jsx
+++ src/projects/weather-app-v2.0/Components/SearchForm.jsx
@@ -2,14 +2,24 @@ import React, { useState } from 'react';
 import './SearchForm.css';
 
 const SearchForm = (props) => {
+  const [ location, setLocation ] = useState(props.location);
+  const locationChangeHandler = (event) => {
+    setLocation(e.target.value);
+  }
 
     return (
       <form>
-        <div>
-          Search for a location in Australia
+        <div className='searchform'>
+          <label>Search for a location</label>
+          <input type='text' placeholder='Gold Coast' onChange={ locationChangeHandler } value={ location } />
+          <div className='searchform-button'>
+            <button type='submit' className='button'>Search</button>
+          </div>
         </div>
       </form>
 
       
     )
 };
+
+export default SearchForm;
