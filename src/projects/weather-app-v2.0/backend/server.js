ae0af18673743db74f3185a0e3b6084a0364b099
teejay327 <mrenergy@hotmail.com>
2024-03-11 21:56:51 +1000

    Modified env and server files to allow concurrentl to work

--- src/projects/weather-app-v2.0/backend/server.js
+++ src/projects/weather-app-v2.0/backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = 5000;
+console.log(port)
 
 const app = express();
 app.get('/', (req,res) => {
