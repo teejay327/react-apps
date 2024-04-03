c2ed1624cd38fc9e4e142999cfcaa6d600882d2e
teejay327 <mrenergy@hotmail.com>
2024-04-03 23:49:12 +1000

    Added user routes

--- src/projects/weather-app-v2.0/backend/controllers/users-controller.js
+++ src/projects/weather-app-v2.0/backend/controllers/users-controller.js
@@ -1,17 +1,49 @@
 import express from 'express';
-
+import{ v4 as uuidv4 } from '../uuid'
 import HttpError from '../models/http-error';
 
+const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Tony Jones',
+    email: 'test@test.com',
+    password: 'tester'
+  },
+  {
+    id: 'u2',
+    name: 'Dasa Jones',
+    email: 'test2@test.com',
+    password: 'tester2'
+  }
+]
 
 const getUsers = (req, res, next) => {
-
+  res.json({ users: DUMMY_USERS })
 };
 
-const signup = (req, res, next) {
+const signup = (req, res, next) => {
+  const { name, email, password} = req.body;
 
+  const createdUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser })
 };
 
-const login = (req, res, next) {
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((user) => user.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError('Incorrect username or password', 401);
+  }
+  res.json({ message: 'Logged in!'});
 
 };
 
--- src/projects/weather-app-v2.0/backend/server.js
+++ src/projects/weather-app-v2.0/backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 import DATA from './data.js';
 console.log(DATA);
 //import router from './routes/users-routes.js';
+import usersRoutes from './routes/users-routes.js';
 
 const port = 5000;
 console.log(port)
@@ -17,6 +18,16 @@ app.get('/', (req,res) => {
 
 app.get('/api/DATA', (req,res) => {
   res.json(DATA);
+});
+
+app.use('api/users', usersRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({ message: error.message || 'Unknown error occurred' });
 });
 
 // start the server
