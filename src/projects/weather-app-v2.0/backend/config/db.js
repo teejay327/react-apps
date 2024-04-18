c0d47c216913c25c5980770218c3939ded9d5979
teejay327 <mrenergy@hotmail.com>
2024-04-18 23:39:18 +1000

    Added mongoose in db.js

--- src/projects/weather-app-v2.0/backend/config/db.js
+++ src/projects/weather-app-v2.0/backend/config/db.js
@@ -5,8 +5,8 @@ const connectDB = async() => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${ error.message }`;
-    process.exit(1);)
+    console.log(`Error: ${ error.message }`);
+    process.exit(1);
   }
 }
 
--- src/projects/weather-app-v2.0/backend/server.js
+++ src/projects/weather-app-v2.0/backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
 dotenv.config();
+
+connectDB();
 
 import DATA from './data.js';
 console.log(DATA);
