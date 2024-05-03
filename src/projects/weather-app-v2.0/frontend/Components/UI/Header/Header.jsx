857f033ef35cf41a8328499490c698aa98c212a0
teejay327 <mrenergy@hotmail.com>
2024-05-03 23:55:16 +1000

    Added menu button to header

--- src/projects/weather-app-v2.0/frontend/Components/UI/Header/Header.jsx
+++ src/projects/weather-app-v2.0/frontend/Components/UI/Header/Header.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 import sunImg from '../../../../assets/sunny.png';
 import './Header.css';
 
@@ -5,6 +8,11 @@ const Header = () => {
 
   return (
     <div className='header'>
+      <button className='nav-menu-btn'>
+        <span />
+        <span />
+        <span />
+      </button>
       <img src={ sunImg } alt="Sun weather" />
       <h1>WeatherLink</h1>
     </div>
