f42b78e9432943988cf6fe8979e0012d4365dbda
teejay327 <mrenergy@hotmail.com>
2024-05-04 00:22:54 +1000

    Added changes in header for smaller viewports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+
 import WeatherApp from './projects/weather-app-v2.0/frontend/Components/WeatherApp.jsx';
 import Header from './projects/weather-app-v2.0/frontend/Components/UI/Header/Header.jsx';
 import Navbar from './projects/weather-app-v2.0/frontend/Components/UI/Navbar/Navbar.jsx';
 import SearchForm from './projects/weather-app-v2.0/frontend/Components/SearchForm.jsx';
-import { BrowserRouter } from 'react-router-dom';
+
 import './projects/weather-app-v2.0/frontend/Components/UI/Header/Header.css';
 import './App.css';
 
