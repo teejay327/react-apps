5d59aba4625c3c9532661bc0b749a4a5f108a175
teejay327 <mrenergy@hotmail.com>
2024-05-16 23:43:52 +1000

    Added Today page and fixed routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import WeatherApp from './projects/weather-app-v2.0/frontend/Components/WeatherApp.jsx';
 import Header from './projects/weather-app-v2.0/frontend/Components/UI/Header/Header.jsx';
 import SearchForm from './projects/weather-app-v2.0/frontend/Components/SearchForm.jsx';
-import Location from './'
+import Location from './projects/weather-app-v2.0/frontend/Components/Users/pages/Location.js';
+import Today from './projects/weather-app-v2.0/frontend/Components/Users/pages/Today.js';
 import Login from './projects/weather-app-v2.0/frontend/Components/Users/pages/Login.js';
 
 import './projects/weather-app-v2.0/frontend/Components/UI/Header/Header.css';
@@ -15,10 +16,16 @@ const App = () => {
       <div className='logo-title'>
         <Header />
       </div>
-      <div className='pages-container'>
-        <Location />
-        <Login />
-      </div>
+
+      <Router>
+        <div className='pages-container'>
+          <Routes>
+            <Route exact path="/location" element={ <Location /> } />
+            <Route exact path="/today" element={ <Today /> } />
+          </Routes>
+        </div>
+      </Router>
+
       <div>
         <SearchForm />
         <WeatherApp /> 
