9f08e2776efca3568ec234bab6edafb0bc9c4683
teejay327 <mrenergy@hotmail.com>
2024-05-27 23:57:37 +1000

    Added custom form component

--- src/projects/weather-app-v2.0/frontend/Components/shared/FormElements/Input.jsx
+++ src/projects/weather-app-v2.0/frontend/Components/shared/FormElements/Input.jsx
@@ -2,17 +2,41 @@ import React, { useReducer } from 'react';
 
 import './Input.css';
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: true
+      };
+    default:
+      return state;
+  }
+};
+
 const Input = props => {
-  useReducer();
-  console.log(props);
+  const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false });
+
   const changeHandler = event => {
+    dispatch({ type: 'CHANGE', val: event.target.value });
+  }
 
-}
+  const element = 
+    <input 
+      type={ props.type } 
+      placeholder={ props.placeholder }
+      onChange={ changeHandler }
+      value={ inputState.value }
+    />
 
-  return <div className={'form-template'}>
-    <label>{ props.label } </label>
-    <input type={ props.type } placeholder={ props.placeholder } />
-  </div>
+  return (
+    <div className={`form-template ${!inputState.isValid && 'form-invalid' }`}>
+      <label>{ props.label } </label>
+      { element }
+      { !inputState.isValid && <p>Invalid </p> }
+    </div>
+  )
 };
 
 export default Input;
