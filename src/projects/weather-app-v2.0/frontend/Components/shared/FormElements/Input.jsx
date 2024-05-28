d8c04e6c5aa263c7e1c2513fff92410dc8d29e04
teejay327 <mrenergy@hotmail.com>
2024-05-28 23:43:07 +1000

    Modified Input.jsx component to check for valid form input

--- src/projects/weather-app-v2.0/frontend/Components/shared/FormElements/Input.jsx
+++ src/projects/weather-app-v2.0/frontend/Components/shared/FormElements/Input.jsx
@@ -1,4 +1,5 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import { validate } from '../util/validators.js';
 
 import './Input.css';
 
@@ -8,33 +9,59 @@ const inputReducer = (state, action) => {
       return {
         ...state,
         value: action.val,
-        isValid: true
+        isValid: validate(action.val, action.validators)
       };
+    case 'TOUCH':
+      return {
+        ...state,
+        isTouched: true
+      }
     default:
       return state;
   }
 };
 
 const Input = props => {
-  const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false });
+  const [inputState, dispatch] = useReducer(inputReducer, { 
+    value: '', 
+    isTouched: false,
+    isValid: false 
+  });
+
+  //const { value, isValid } = inputState;
+
+  useEffect(() => {
+    props.onInput(inputState.value, inputState.isValid)
+  }, [value, isValid, onInput]);
 
   const changeHandler = event => {
-    dispatch({ type: 'CHANGE', val: event.target.value });
-  }
+    dispatch({ 
+      type: 'CHANGE', 
+      val: event.target.value, 
+      validators: props.validators 
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: 'TOUCH'
+    });
+  };
 
   const element = 
     <input 
       type={ props.type } 
       placeholder={ props.placeholder }
       onChange={ changeHandler }
+      onBlur={ touchHandler }
       value={ inputState.value }
     />
 
   return (
-    <div className={`form-template ${!inputState.isValid && 'form-invalid' }`}>
+    <div className={`form-template ${!inputState.isValid && inputState.isTouched && 'form-invalid' }`}>
       <label>{ props.label } </label>
       { element }
-      { !inputState.isValid && <p>Invalid </p> }
+      { !inputState.isValid && inputState.isTouched && <p>Invalid input</p> }
     </div>
   )
 };
