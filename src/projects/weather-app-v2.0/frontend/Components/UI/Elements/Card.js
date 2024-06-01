21db33ba86b849d1cfa3916b8903ddf09eb4a129
teejay327 <mrenergy@hotmail.com>
2024-06-01 23:35:12 +1000

    Added error validation for Login.css

--- src/projects/weather-app-v2.0/frontend/Components/UI/Elements/Card.js
+++ src/projects/weather-app-v2.0/frontend/Components/UI/Elements/Card.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import './Card.css';
 
-// Take out unless needed - import into UserItem.js
-
 const Card = props => {
   return (
     <div className={`card ${props.className}`} style={props.style}>
